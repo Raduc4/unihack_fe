f384f9f6aae6fabdf58ee6281c3f05124f519888
Chiril Gorbenco <chiril.gorbenco@student.upt.ro>
2023-11-18 01:32:18 +0200

    What

--- sections/forms/forms.stories.tsx
+++ sections/forms/forms.stories.tsx
@@ -1,8 +1,8 @@
 import { Meta, Story } from '@storybook/react';
-import Hero from './hero.section';
+import Forms from './forms.section';
 
 export default {
   title: 'Forms page section',
 } as Meta;
 
-export const FormsSection: Story = () => <Hero />;
+export const FormsSection: Story = () => <Forms />;
